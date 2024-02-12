5a09ec1c88fe72bb7fa0bec0a99e110f118dc59a
amanjot <amanmatreja@gmail.com>
2024-02-12 13:35:01 +0530

    fix

--- start_server.sh
+++ start_server.sh
@@ -17,7 +17,7 @@ then
     echo "booting" | tee -a /root/debug.log
     mkdir /home/workspace
     cd /home/workspace
-    git clone https://github.com/r3sist-uniq/dreamgaussian_autoscaler.git vast-pyworker
+    git clone https://github.com/r3sist-uniq/vast-pyworker.git 
     
     python3 -m venv /home/workspace/worker-env
     source /home/workspace/worker-env/bin/activate
